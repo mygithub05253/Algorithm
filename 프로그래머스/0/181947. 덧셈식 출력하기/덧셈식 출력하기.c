cccd9bf5ce8f466b3a484f7511a5f588c85cb874
mygithub05253 <kik328288@gmail.com>
2025-03-12 14:28:38 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 1.62 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void) 
+{
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d", a, b, a + b);
+    return 0;
+}
