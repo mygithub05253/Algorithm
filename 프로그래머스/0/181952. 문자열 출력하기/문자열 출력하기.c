8e1fd0826b133037135e5850171e9e856f0cc3a8
mygithub05253 <kik328288@gmail.com>
2025-03-12 14:29:14 +0900

    [level 0] Title: 문자열 출력하기, Time: 2.22 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define LEN_INPUT 1000001
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+    char str[LEN_INPUT];
+    scanf("%s", str);
+    printf("%s", str);
+
+    return 0;
+}
